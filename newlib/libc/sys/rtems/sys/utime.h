45024d1fe1b738d84208a74808401abecf215dba
Jeff Johnston <jjohnstn@redhat.com>
2002-11-07 19:27:36 +0000

    2002-11-07  Joel Sherrill <joel@OARcorp.com>
            * libc/sys/rtems/machine: New directory.
            * libc/sys/rtems/machine/limits.h, libc/sys/rtems/machine/param.h,
            libc/sys/rtems/sys/param.h, libc/sys/rtems/sys/syslimits.h,
            libc/sys/rtems/sys/utime.h: New files added to make *-rtems newlib
            targets more BSD like when installed without requiring files to
            be overwritten at install point when RTEMS itself is installed.
            * Makefile.am: Pick up system dependent machine .h files such as
            might be found on a BSD-ish system.
            * Makefile.in: Regenerate.
            * libc/include/machine/types.h: When on an RTEMS target, define a
            few BSD flavor types.

---
+++ newlib/libc/sys/rtems/sys/utime.h
@@ -0,0 +1,32 @@
+/*
+ *  $Id: utime.h,v 1.1 1999/03/24 22:52:47 joel Exp $
+ */
+
+#ifndef __UTIME_h__
+#define __UTIME_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  POSIX 1003.1b 5.6.6 Set File Access and Modification Times
+ */
+
+struct utimbuf {
+  time_t  actime;   /* Access time */
+  time_t  modtime;  /* Modification time */
+};
+
+/* Functions */
+
+int utime(
+  const char           *path,
+  const struct utimbuf *times
+);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* _SYS_UTIME_H */
