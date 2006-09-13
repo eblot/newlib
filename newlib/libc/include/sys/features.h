9d8565b42e4b68d8cba5d47d8c27ab4ee96bcdcb
Jeff Johnston <jjohnstn@redhat.com>
2006-09-13 22:09:27 +0000

    2006-09-13  Joel Sherrill <joel@oarcorp.com>
            * libc/include/pthread.h: Add pthread barriers,
            rwlocks, and spinlocks.  Change const to
            _CONST and prefix parameter names with
            an underscore.
            * libc/include/sys/types.h: Add id and attribute
            types for barriers, wrlocks, and spinlocks.
            * libc/include/sys/features.h: Enable barriers,
            wrlocks, and spinlocks for RTEMS.

--- newlib/libc/include/sys/features.h
+++ newlib/libc/include/sys/features.h
@@ -15,7 +15,7 @@
  *  OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY OF THIS
  *  SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
  *
- *  $Id: features.h,v 1.6 2004/02/09 02:22:01 cgf Exp $
+ *  $Id: features.h,v 1.7 2005/10/18 18:39:46 corinna Exp $
  */
 
 #ifndef _SYS_FEATURES_H
@@ -45,6 +45,9 @@ extern "C" {
 #define _POSIX_SHARED_MEMORY_OBJECTS	1
 #define _POSIX_SYNCHRONIZED_IO		1
 #define _POSIX_TIMERS			1
+#define _POSIX_BARRIERS                 200112L
+#define _POSIX_READER_WRITER_LOCKS      200112L
+#define _POSIX_SPIN_LOCKS               200112L
 
 
 /* In P1003.1b but defined by drafts at least as early as P1003.1c/D10  */
