97ad296201658f010f0af0e9bbe5ecb6653c2015
Emmanuel Blot <emmanuel.blot@free.fr>
2013-07-02 14:13:44 +0200

    Fix warning attribute definition for Clang compiler

--- newlib/libc/include/stdlib.h
+++ newlib/libc/include/stdlib.h
@@ -25,6 +25,12 @@
 #include <cygwin/stdlib.h>
 #endif
 
+#ifdef __clang__
+#define _DEPRECATE(_msg_) _ATTRIBUTE ((deprecated (_msg_)))
+#else
+#define _DEPRECATE(_msg_) _ATTRIBUTE ((__warning__ (_msg_)))
+#endif
+
 _BEGIN_STD_C
 
 typedef struct 
@@ -104,14 +110,14 @@ int	_EXFUN(mkostemp,(char *, int));
 int	_EXFUN(mkostemps,(char *, int, int));
 int	_EXFUN(mkstemp,(char *));
 int	_EXFUN(mkstemps,(char *, int));
-char *	_EXFUN(mktemp,(char *) _ATTRIBUTE ((__warning__ ("the use of `mktemp' is dangerous; use `mkstemp' instead"))));
+char *	_EXFUN(mktemp,(char *) _DEPRECATE("the use of `mktemp' is dangerous; use `mkstemp' instead"));
 #endif
 char *	_EXFUN(_mkdtemp_r, (struct _reent *, char *));
 int	_EXFUN(_mkostemp_r, (struct _reent *, char *, int));
 int	_EXFUN(_mkostemps_r, (struct _reent *, char *, int, int));
 int	_EXFUN(_mkstemp_r, (struct _reent *, char *));
 int	_EXFUN(_mkstemps_r, (struct _reent *, char *, int));
-char *	_EXFUN(_mktemp_r, (struct _reent *, char *) _ATTRIBUTE ((__warning__ ("the use of `mktemp' is dangerous; use `mkstemp' instead"))));
+char *	_EXFUN(_mktemp_r, (struct _reent *, char *) _DEPRECATE("the use of `mktemp' is dangerous; use `mkstemp' instead"));
 #endif
 _VOID	_EXFUN(qsort,(_PTR __base, size_t __nmemb, size_t __size, int(*_compar)(const _PTR, const _PTR)));
 int	_EXFUN(rand,(_VOID));
